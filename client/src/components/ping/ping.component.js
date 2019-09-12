351f7cd587def1083a1f34223592bf793bb32655
Nick Burnard <nich2jb@gmail.com>
2019-09-12 11:38:34 +1000

    fix styling issues

--- client/src/components/ping/ping.component.js
+++ client/src/components/ping/ping.component.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+
 function Ping() {
-    const [pong, setPong] = useState('pending')
+  const [pong, setPong] = useState('pending')
 
-    useEffect(() => {
-        axios.get('api/ping')
-            .then((response) => {
-                setPong(response.data.message)
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
+  useEffect(() => {
+    axios.get('api/ping')
+      .then((response) => {
+      setPong(response.data.message)
     })
+      .catch(function (error) {
+      console.log(error);
+    });
 
-    return(
-        <h1>Ping {pong}</h1>
-    )
+  })
+
+  return(
+    <h1>Ping {pong}</h1>
+  )
 }
 
 export default Ping;
